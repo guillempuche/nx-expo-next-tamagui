bf3c921dcd0be4c40f1a58c15c2bc68643686230
Guillem Puche <3519924+guillempuche@users.noreply.github.com>
2024-02-12 20:59:46 +0100

    build(welcome): add package.json

--- packages/app/welcome/src/layout.web.tsx
+++ packages/app/welcome/src/layout.web.tsx
@@ -1,22 +1,22 @@
-import { ScrollView, YStack, Text } from '@nx-expo-next-tamagui/components';
+import { ScrollView, YStack, Text } from "@nx-expo-next-tamagui/components";
 
 export function WelcomeLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <YStack f={1} backgroundColor="$background">
-      <ScrollView>
-        <YStack padding="$4" space="$4" jc="center">
-          {children}
-        </YStack>
-      </ScrollView>
-      <Footer />
-    </YStack>
-  );
+	return (
+		<YStack f={1} backgroundColor="$background">
+			<ScrollView>
+				<YStack padding="$4" space="$4" jc="center">
+					{children}
+				</YStack>
+			</ScrollView>
+			<Footer />
+		</YStack>
+	);
 }
 
 function Footer() {
-  return (
-    <YStack padding="$4" ai="center">
-      <Text>Footer Content</Text>
-    </YStack>
-  );
+	return (
+		<YStack padding="$4" ai="center">
+			<Text>Footer Content</Text>
+		</YStack>
+	);
 }
