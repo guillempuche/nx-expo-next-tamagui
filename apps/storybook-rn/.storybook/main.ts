5a99742d6eed9be987141f327855e8bb4fbdab6d
Guillem Puche <3519924+guillempuche@users.noreply.github.com>
2024-02-13 20:27:15 +0100

    feat: add Storybook for React Native

--- apps/storybook-rn/.storybook/main.ts
+++ apps/storybook-rn/.storybook/main.ts
@@ -1,17 +1,16 @@
-// import type { StorybookConfig } from '@storybook/react-vite';
 import type { StorybookConfig } from '@storybook/react-native'
-
-import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin'
-import { mergeConfig } from 'vite'
+// import type { StorybookConfig } from '@storybook/react-vite';
+// import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin'
+// import { mergeConfig } from 'vite'
 
 const config: StorybookConfig = {
 	stories: ['../../../packages/components/**/*.stories.@(ts|tsx|mdx)'],
 	addons: ['@storybook/addon-essentials'],
+
 	// framework: {
 	//   name: '@storybook/react-vite',
 	//   options: {},
 	// },
-
 	// viteFinal: async (config) =>
 	//   mergeConfig(config, {
 	//     plugins: [nxViteTsPaths()],
